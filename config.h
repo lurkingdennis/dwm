37cf3a917c74b355857802a4d337f9f2c1272c9a
Dennis C. <dpclements@proton.me>
2024-09-09 11:19:49 -0700

    changed settings

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh    = 0;       /* horiz outer gap between window
 static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar. default is 2 */
 static const int vertpadbar         = 4;        /* vertical padding for statusbar. default is 0 */
 static const char *fonts[]          = { "Terminess Nerd Font:size=9" };
